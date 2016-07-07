04ccda7ae6950da2ab664dcaa8bab716a1707ca6
Jan van Helvoort <janvanhelvoort@outlook.com>
2016-07-07 13:13:31 +0200

    Support atomic block type (#16)

--- src/stateToHTML.js
+++ src/stateToHTML.js
@@ -118,6 +118,8 @@ function getTags(blockType: string): Array<string> {
       return ['blockquote'];
     case BLOCK_TYPE.CODE:
       return ['pre', 'code'];
+	case BLOCK_TYPE.ATOMIC:
+      return ['figure'];
     default:
       return ['p'];
   }
