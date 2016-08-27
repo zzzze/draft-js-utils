46ae977bf8ca668fbb28711781702000b52660b7
Simon Sturmer <sstur@me.com>
2016-08-27 21:40:40 +0700

    Don't break compatibility with Draft 0.7.x

--- src/__tests__/stateToHTML-test.js
+++ src/__tests__/stateToHTML-test.js
@@ -81,10 +81,11 @@ describe('stateToHTML', () => {
   it('should support custom block styles', () => {
     let options = {
       blockStyleFn: (block) => {
-        if (block.getData().get('alignment')) {
+        let alignment = block.getData ? block.getData().get('alignment') : null;
+        if (alignment) {
           return {
             style: {
-              textAlign: block.getData().get('alignment'),
+              textAlign: alignment,
             },
           };
         }
@@ -94,6 +95,10 @@ describe('stateToHTML', () => {
       // <h1 style="text-align: left;">Hello <em>world</em>.</h1>
       {"entityMap":{},"blocks":[{"data":{"alignment":"left"},"key":"dn025","text":"Hello world.","type":"header-one","depth":0,"inlineStyleRanges":[{"offset":6,"length":5,"style":"ITALIC"}],"entityRanges":[]}]} // eslint-disable-line
     );
+    if (contentState1.getFirstBlock().getData == null) {
+      // Older DraftJS does not support block.getData()
+      return;
+    }
     expect(stateToHTML(contentState1, options)).toBe(
       '<h1 style="text-align: left">Hello <em>world</em>.</h1>'
     );
