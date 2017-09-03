1d2cdcd8ed2a3cbe66e7edcbbc6e0fcca9e258dc
Simon Sturmer <sstur@me.com>
2017-09-03 10:39:36 +0700

    import synthetic-dom

---
+++ packages/synthetic-dom/src/SyntheticDOM.js
@@ -0,0 +1,136 @@
+/* @flow */
+
+type Attr = {name: string; value: string};
+type AttrList = Array<Attr>;
+type MapList = {[key: string]: boolean};
+
+const EMPTY_ATTR_LIST: AttrList = [];
+
+export const NODE_TYPE_ELEMENT = 1;
+export const NODE_TYPE_TEXT = 3;
+export const NODE_TYPE_FRAGMENT = 11;
+export const SELF_CLOSING: MapList = {
+  area: true, base: true, br: true, col: true, embed: true, hr: true, img: true,
+  input: true, keygen: true, link: true, meta: true, param: true, source: true,
+  track: true, wbr: true,
+};
+
+export class Node {
+  nodeType: number;
+  nodeName: string;
+  nodeValue: string;
+}
+
+export class TextNode extends Node {
+  constructor(value: string) {
+    super(...arguments);
+    this.nodeType = NODE_TYPE_TEXT;
+    this.nodeName = '#text';
+    this.nodeValue = value;
+  }
+
+  toString(): string {
+    return escape(this.nodeValue);
+  }
+}
+
+export class ElementNode extends Node {
+  childNodes: Array<Node>;
+  attributes: AttrList;
+  attrMap: Map<string, Attr>;
+  isSelfClosing: boolean;
+
+  constructor(name: string, attributes: ?AttrList, childNodes: ?Array<Node>) {
+    super(...arguments);
+    if (attributes == null) {
+      attributes = EMPTY_ATTR_LIST;
+    }
+    let isSelfClosing = (SELF_CLOSING[name] === true);
+    this.nodeType = NODE_TYPE_ELEMENT;
+    this.nodeName = name;
+    this.attributes = attributes;
+    this.attrMap = new Map(attributes.map((attr) => [attr.name, attr]));
+    this.childNodes = [];
+    this.isSelfClosing = isSelfClosing;
+    if (!isSelfClosing && childNodes) {
+      childNodes.forEach(this.appendChild, this);
+    }
+  }
+
+  appendChild(node: Node) {
+    if (node.nodeType === NODE_TYPE_FRAGMENT) {
+      if (node.childNodes != null) {
+        // $FlowIssue - Flow doesn't realize that node is a FragmentNode.
+        let childNodes: Array<Node> = node.childNodes;
+        this.childNodes.push(...childNodes);
+      }
+    } else {
+      this.childNodes.push(node);
+    }
+  }
+
+  getAttribute(name: string): ?string {
+    let attr = this.attrMap.get(name);
+    if (attr) {
+      return attr.value;
+    }
+  }
+
+  toString(isXHTML: ?boolean): string {
+    let attributes = [];
+    for (let {name, value} of this.attributes) {
+      attributes.push(name + (value ? '="' + escapeAttr(value) + '"' : ''));
+    }
+    let attrString = attributes.length ? ' ' + attributes.join(' ') : '';
+    if (this.isSelfClosing) {
+      return '<' + this.nodeName + attrString + (isXHTML ? '/>' : '>');
+    }
+    let childNodes = this.childNodes.map((node) => node.toString(isXHTML)).join('');
+    return '<' + this.nodeName + attrString + '>' + childNodes + '</' + this.nodeName + '>';
+  }
+}
+
+export class FragmentNode extends Node {
+  childNodes: Array<Node>;
+
+  constructor(childNodes: Array<Node>) {
+    super(...arguments);
+    this.nodeType = NODE_TYPE_FRAGMENT;
+    this.childNodes = [];
+    if (childNodes) {
+      childNodes.forEach(this.appendChild, this);
+    }
+  }
+
+  appendChild(node: Node) {
+    if (node.nodeType === NODE_TYPE_FRAGMENT) {
+      if (node.childNodes != null) {
+        // $FlowIssue - Flow doesn't realize that node is a FragmentNode.
+        let childNodes: Array<Node> = node.childNodes;
+        this.childNodes.push(...childNodes);
+      }
+    } else {
+      this.childNodes.push(node);
+    }
+  }
+
+  toString(isXHTML: boolean): string {
+    return this.childNodes.map((node) => node.toString(isXHTML)).join('');
+  }
+}
+
+function escape(html: string): string {
+  return html
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
+
+function escapeAttr(html: string): string {
+  return html
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
---
+++ packages/synthetic-dom/src/__tests__/SyntheticDOM-test.js
@@ -0,0 +1,68 @@
+/* @flow */
+const {describe, it} = global;
+import expect from 'expect';
+import {
+  TextNode,
+  NODE_TYPE_TEXT,
+  ElementNode,
+  NODE_TYPE_ELEMENT,
+} from '../SyntheticDOM';
+
+describe('Text Nodes', () => {
+  let textNode = new TextNode('Hello World');
+  it('should get created properly', () => {
+    expect(textNode.nodeType).toBe(NODE_TYPE_TEXT);
+    expect(textNode.nodeName).toBe('#text');
+    expect(textNode.nodeValue).toBe('Hello World');
+  });
+});
+
+describe('Elements', () => {
+  it('should accept empty attributes and empty children', () => {
+    let element = new ElementNode('div', [], []);
+    expect(element.nodeType).toBe(NODE_TYPE_ELEMENT);
+    expect(element.nodeName).toBe('div');
+    expect(element.attributes).toBeAn(Array);
+    expect(element.attributes.length).toBe(0);
+    expect(element.childNodes).toEqual([]);
+  });
+
+  it('should accept null attributes and some children', () => {
+    let textNode = new TextNode('Hello World');
+    let element = new ElementNode('div', null, [textNode]);
+    expect(element.attributes).toBeAn(Array);
+    expect(element.attributes.length).toBe(0);
+    expect(element.toString()).toBe('<div>Hello World</div>');
+  });
+
+  it('should accept null attributes and null children', () => {
+    let element = new ElementNode('div');
+    expect(element.attributes).toBeAn(Array);
+    expect(element.attributes.length).toBe(0);
+    expect(element.toString()).toBe('<div></div>');
+  });
+
+  it('should ignore children for self-closing (void) tags', () => {
+    let p = new ElementNode('p');
+    let element = new ElementNode('hr', null, [p]);
+    expect(element.childNodes.length).toBe(0);
+    expect(element.toString()).toBe('<hr>');
+  });
+
+  it('should stringify correctly', () => {
+    let br = new ElementNode('br');
+    let p = new ElementNode('p', null, [br, br]);
+    let attrs = [{name: 'className', value: 'foo'}];
+    let element = new ElementNode('div', attrs, [p]);
+    expect(element.childNodes.length).toBe(1);
+    let firstChild = element.childNodes[0];
+    // Weird branching to make Flow happy.
+    if (firstChild instanceof ElementNode) {
+      expect(firstChild.childNodes.length).toBe(2);
+    } else {
+      expect(firstChild).toBeAn(ElementNode);
+    }
+    expect(element.toString()).toBe('<div className="foo"><p><br><br></p></div>');
+    expect(element.toString(true)).toBe('<div className="foo"><p><br/><br/></p></div>');
+  });
+});
