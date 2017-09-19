9f778a517347d626ce04999141cf63f2d67fcba1
Simon Sturmer <sstur@me.com>
2017-09-19 07:13:14 +0700

    [import-markdown] Fix code blocks (#98)

--- packages/draft-js-import-markdown/src/__tests__/stateFromMarkdown-test.js
+++ packages/draft-js-import-markdown/src/__tests__/stateFromMarkdown-test.js
@@ -21,11 +21,34 @@ describe('stateFromMarkdown', () => {
       },
     ]);
   });
+  it('should correctly move code blocks', () => {
+    let codeMarkdown = "```\nconst a = 'b'\n```";
+    let contentState = stateFromMarkdown(codeMarkdown);
+    let rawContentState = convertToRaw(contentState);
+    let blocks = removeKeys(rawContentState.blocks);
+    expect(blocks).toEqual([
+      {
+        text: "const a = 'b'",
+        type: 'code-block',
+        data: {},
+        depth: 0,
+        inlineStyleRanges: [
+          {
+            length: 13,
+            offset: 0,
+            style: 'CODE',
+          },
+        ],
+        entityRanges: [],
+      },
+    ]);
+  });
 });
 
 function removeKeys(blocks) {
   return blocks.map((block) => {
-    let {key, ...other} = block; // eslint-disable-line no-unused-vars
+    // eslint-disable-next-line no-unused-vars
+    let {key, ...other} = block;
     return other;
   });
 }
