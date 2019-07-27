84abcbb0c4905210db00f8023a9dc4560a1365b0
Bogdan Chadkin <trysound@yandex.ru>
2019-07-27 19:05:37 +0200

    Add esm support (#191)
    This is useful for bundlers like rollup which does not support commonjs
    our of the box. It's also allows webpack to not wrap each module with
    commonjs runtime and enable scope hoisting.
    
    AST plugins already support it
    https://github.com/icelab/draft-js-ast-importer/blob/master/package.json#L6
    https://github.com/icelab/draft-js-ast-exporter/blob/master/package.json#L6

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['@babel/env', '@babel/react', '@babel/flow'],
   plugins: ['@babel/proposal-class-properties'],
+  env: {
+    esm: {
+      presets: [['@babel/env', {modules: false}]],
+    },
+  },
 };
