35937c2c31979737476ded28de5141c51ecae100
Ash Browning <ambrowning89@gmail.com>
2020-08-26 19:53:29 +0100

    fix(markdown): trimming styled text (#209)
    * fix(markdown): trimming styled text
    
    If a word including its surrounding whitespace is formatted, the
    markdown output (while valid) will not correctly render the formatting.
    
    * fix(markdown): using trimLeft/Right vs trimStart/End for node v8
    
    * Check for empty string before applying inline styles
    
    * Don't need to trim twice

--- packages/draft-js-export-markdown/src/stateToMarkdown.js
+++ packages/draft-js-export-markdown/src/stateToMarkdown.js
@@ -218,13 +218,24 @@ class MarkupGenerator {
             if (blockType === BLOCK_TYPE.CODE) {
               return content;
             }
+
+            const contentLength = content.length;
+            content = content.trimLeft();
+            const leftPad = contentLength - content.length;
+            content = content.trimRight();
+            const rightPad = contentLength - content.length - leftPad;
+if (!content.length) {
+  // Return the original text if there is nothing left after trimming.
+  return text;
+}
+
             // NOTE: We attempt some basic character escaping here, although
             // I don't know if escape sequences are really valid in markdown,
             // there's not a canonical spec to lean on.
             if (style.has(CODE)) {
               return '`' + encodeCode(content) + '`';
             }
-            content = encodeContent(text);
+            content = encodeContent(content);
             if (style.has(BOLD)) {
               content = `**${content}**`;
             }
@@ -239,7 +250,8 @@ class MarkupGenerator {
               // TODO: encode `~`?
               content = `~~${content}~~`;
             }
-            return content;
+            content = content.padStart(content.length + leftPad);
+            return content.padEnd(content.length + rightPad);
           })
           .join('');
         let entity = entityKey ? contentState.getEntity(entityKey) : null;
